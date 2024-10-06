074e6e0210a7e47ea85b9fc0eaade07f4c347d13
bala <sokukumar678@gmail.com>
2024-10-06 19:20:15 +0530

    adding a new class with two member funtion

--- E-Wallet-Management-System.cpp
+++ E-Wallet-Management-System.cpp
@@ -11,6 +11,25 @@ class User{
         balance=0; 
     }
 };
+class EWallet{
+    private:
+    unordered_map<string,User>users;
+    public:
+    void registerUser(const string &name,const string &email){
+        if(users.find(email)==users.end()){
+            users[email]=User(name,email);
+            cout<<"User registered sucessfully"<<endl;
+        }
+        else cout<<"User exist already"<<endl;
+    }
+    void addMoney(const string email,double amt){
+        if(users.find(email)!=users.end()){
+            users[email].balance+=amt;
+            cout<<"Added $"<<amt<<"to"<<email<<"s' wallet"<<endl;
+        }
+        else cout<<"user not found"<<endl;
+    }
+};
 int main(){
     
     return 0;
